8aa6530e5983ea1193e9d4141db356b24c2ee008
Javin2005 <163754193+Javin2005@users.noreply.github.com>
2026-07-06 02:28:40 -0500

    Changed the main.cpp file to be more detailed and in english

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include "Protocol.h"
 
+using namespace std;
+
 int main()
 {
-    std::cout << "PingPacket size: " << sizeof(PingPacket) << " bytes" << std::endl;
+    cout << "Project Chronos: Server Initialization" << endl;
+
+    cout << "Size of NetHeader: " << sizeof(NetHeader) << " bytes (Expected: 5)" << endl;
+    cout << "Size of PingPocket: " << sizeof(PingPacket) << " bytes (Expected 13)" << endl;
+
+    if (sizeof(NetHeader) != 5)
+    {
+        cerr << "CRITICAL: Struct packing failed for NetHeader!" << endl;
+        return 1;
+    }
+
+    cout << "Protocol verification successful. Server ready for implementation" << endl;
     return 0;
 }
