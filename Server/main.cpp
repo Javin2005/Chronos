4ac872757a03429412158a378585b40de52e214c
Javin2005 <163754193+Javin2005@users.noreply.github.com>
2026-07-06 14:47:30 -0500

    feat added actual port listining functionality to main.cpp

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,21 +1,47 @@
 #include <iostream>
-#include "Protocol.h"
+#include "Protocol.hpp"
+#include <asio.hpp>
 
 using namespace std;
+using namespace asio;
+using asio::ip::udp;
 
 int main()
 {
-    cout << "Project Chronos: Server Initialization" << endl;
-
-    cout << "Size of NetHeader: " << sizeof(NetHeader) << " bytes (Expected: 5)" << endl;
-    cout << "Size of PingPocket: " << sizeof(PingPacket) << " bytes (Expected 13)" << endl;
-
-    if (sizeof(NetHeader) != 5)
+    try
     {
-        cerr << "CRITICAL: Struct packing failed for NetHeader!" << endl;
-        return 1;
-    }
+        io_context io_context;
 
-    cout << "Protocol verification successful. Server ready for implementation" << endl;
+        udp::socket socket(io_context, udp::endpoint(udp::v4(), 9999));
+
+        cout << "Server is listening on port 9999..." << endl;
+
+        while (true)
+        {
+
+            char recv_buf[1024];
+            udp::endpoint remote_endpoint;
+
+            size_t bytes_received = socket.receive_from(
+                buffer(recv_buf),
+                remote_endpoint);
+
+            cout << "Received " << bytes_received << " bytes from " << remote_endpoint << endl;
+
+            if (bytes_received >= sizeof(NetHeader))
+            {
+                NetHeader *header = reinterpret_cast<NetHeader *>(recv_buf);
+
+                if (header->type == PacketType::Ping)
+                {
+                    cout << "It's a Ping! Sequence: " << header->sequence << endl;
+                }
+            }
+        }
+    }
+    catch (exception &e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
     return 0;
 }
